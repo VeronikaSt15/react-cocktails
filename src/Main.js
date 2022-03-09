028c23302a7f3717d6b1d7f45600e1514df01f02
Veronika <verastupak15@gmail.com>
2022-03-09 15:05:47 +0200

    Initialize project using Create React App

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function Main() {
+    return (
+        <>
+          <h1>This is main page. I will tell you about coctails</h1>
+        </>
+    );
+}
+export default Main;
